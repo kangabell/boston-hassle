2f6ab266ea181a21d748b277912768b196e39f54
K Belardinelli <k@kangabell.co>
2016-11-23 14:45:10 -0500

    set up some basic typography, more semantic markup, etc.

--- partials/article.php
+++ partials/article.php
@@ -1,5 +1,5 @@
 <article>
   <img src="img.png">
-  <h4>Article Title</h4>
+  <h3>Article Title</h3>
   <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
 </article>
---
+++ partials/event-sm.php
@@ -0,0 +1,5 @@
+<article>
+	<p class="meta">11/22</p>
+	<a href="">Dirty Fences, Zip-Tie Handcuffs, The Pragmatics 
+@ Great Scott</a>
+</article>
