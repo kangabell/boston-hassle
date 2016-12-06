9170454ec0f99918a57e1ce595f766e2b2642344
K Belardinelli <k@kangabell.co>
2016-12-06 15:38:33 -0500

    plumbing for Music and Film sections of homepage

--- index.php
+++ index.php
@@ -52,17 +52,28 @@
 
       <div class="music">
         <h2>Music</h2>
-        <?php include 'partials/article.php'; ?>
-        <?php include 'partials/article-sm.php'; ?>
-        <?php include 'partials/article-sm.php'; ?>
+        <?php
+          $loop = new WP_Query( array('posts_per_page'=>3,'cat'=>198) );
+          while ($loop->have_posts()) : 
+            $loop->the_post();
+            if ($loop->current_post == 0) {
+              include 'partials/article.php';
+            } else {
+              include 'partials/article-sm.php';
+            }
+          endwhile; wp_reset_postdata();
+        ?>
       </div>
 
       <div class="film">
         <h2>Film</h2>
-        <?php include 'partials/article-sm.php'; ?>
-        <?php include 'partials/article-sm.php'; ?>
-        <?php include 'partials/article-sm.php'; ?>
-        <?php include 'partials/article-sm.php'; ?>
+        <?php
+          query_posts ('posts_per_page=4&cat=5');
+          if (have_posts()) : while (have_posts()) : the_post();
+            include 'partials/article-sm.php';
+          endwhile; endif;
+          wp_reset_query();
+        ?>
       </div>
 
       <div class="compass">
@@ -76,12 +87,11 @@
     <div class="articles">
       <div>
         <?php 
-          global $query_string;
           query_posts ('posts_per_page=8');
           if (have_posts()) : while (have_posts()) : the_post();
+            include 'partials/article.php';
+          endwhile; endif;
         ?>
-          <?php include 'partials/article.php'; ?>
-        <?php endwhile; endif; ?>
       </div>
       <div class="view-all"><button>View All Posts</button></div>
     </div>
