85739584f7601d9854375a9d3d14e90618c15455
K Belardinelli <k@kangabell.co>
2016-12-06 16:48:16 -0500

    basic footer styling

--- footer.php
+++ footer.php
@@ -1,54 +1,39 @@
   <footer>
     <div>
-      <h4>Header</h4>
-      <ul>
-        <li>Link</li>
-        <li>Link</li>
-
-        <li>Link</li>
-        <li>Link</li>
-      </ul>
-    </div>
-    <div>
-      <h4>Header</h4>
-      <ul>
-        <li>Link</li>
-        <li>Link</li>
-
-        <li>Link</li>
-
-        <li>Link</li>
-
-      </ul>
-    </div>
-    <div>
-      <h4>Header</h4>
-      <ul>
-        <li>Link</li>
-        <li>Link</li>
-
-        <li>Link</li>
-
-        <li>Link</li>
-
-        <li>Link</li>
-      </ul>
-    </div>
-    <div>
-      <h4>Header</h4>
-      <ul>
-        <li>Link</li>
-        <li>Link</li>
-
-        <li>Link</li>
-
-      </ul>
+      <section>
+        <h4>Boston Hassle</h4>
+        <p>
+          The Boston Hassle is an arts and music organization brought to you by the musicians, artists, writers, and enthusiasts in the Boston area. We foster an innovative, compelling, and interconnected music, art, and film community in the greater Boston area through our inclusive grassroots participatory culture.
+          <br />
+          <a href="">Read More</a>
+        </p>
+      </section>
+      <section>
+        <h4>Our Projects</h4>
+        <ul>
+          <li>Link</li>
+          <li>Link</li>
+          <li>Link</li>
+          <li>Link</li>
+        </ul>
+      </section>
+      <section>
+        <h4>Info</h4>
+        <ul>
+          <li>Link</li>
+          <li>Link</li>
+          <li>Link</li>
+          <li>Link</li>
+        </ul>
+      </section>
     </div>
   </footer>
 
   <?php wp_footer(); ?>
 
-  <div class="copyright">&copy; 2016 BRAIN Arts</div>
+  <div class="subfooter">
+    <div>&copy; 2016 BRAIN Arts</div>
+  </div>
 
   </body>
 </html>
