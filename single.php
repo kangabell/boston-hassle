99098e816d3bc2e454c1d0954f349bb715059f9e
K Belardinelli <k@kangabell.co>
2016-12-07 14:09:10 -0500

    begin wordpressy single.php

--- single.php
+++ single.php
@@ -1,19 +1,18 @@
-<main>
-  <article>
-    <h1>Article Title</h1>
-    <p class="meta">Meta, info information</p>
-    <p>    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget velit neque. Quisque non risus ultricies, iaculis massa non, posuere ligula. Nam sodales blandit felis, sed scelerisque sapien tincidunt id. In dui elit, scelerisque eu sapien in, mattis sagittis leo. Nulla eget lacus sapien. Nullam ut est ut augue feugiat posuere vitae id risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam volutpat tellus non leo efficitur euismod. Aenean fermentum elementum metus et hendrerit. Donec porttitor, enim id sollicitudin ullamcorper, eros lectus congue neque, sed consequat tortor augue in lorem. Sed eget est consequat, lobortis ex et, lobortis magna.
-    </p>
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/library/img.png">
-    <p>    Etiam bibendum mattis tortor vitae egestas. In ligula mauris, blandit non congue nec, maximus at eros. In pharetra dapibus velit pellentesque vulputate. Sed eget dolor odio. Curabitur dictum sem eget dolor tincidunt, nec ornare turpis facilisis. Aliquam placerat at libero et condimentum. Suspendisse vulputate sed massa quis egestas. Nam eros diam, sollicitudin vel ultricies ut, mattis dignissim nisl. Duis vel ultrices ante, quis malesuada nunc. Maecenas finibus mattis libero non molestie. Integer accumsan, nulla ut finibus vestibulum, arcu lacus vehicula justo, et fringilla tortor nulla sed nunc. Quisque dolor magna, sodales vel tempus eu, semper sit amet ex. Aenean lectus enim, laoreet elementum tellus nec, rhoncus egestas risus. Integer suscipit commodo risus, ac cursus nisi malesuada non.
-    </p>
-    <p>    Nulla vulputate sapien tellus, a convallis leo condimentum nec. Ut aliquam, urna at sodales imperdiet, velit felis tempor quam, ac auctor ipsum arcu non elit. Sed pulvinar quis turpis a malesuada. Quisque ac nisl eget elit maximus aliquet vulputate et enim. Donec ligula justo, dictum id luctus ut, mollis sed arcu. Morbi a molestie mi. Aliquam scelerisque nisi eu sapien laoreet, ac tincidunt arcu porta. Donec vel felis nisl. Nulla varius enim ac mauris laoreet pretium. Curabitur leo orci, sollicitudin eget molestie luctus, viverra eget urna.
-    </p>
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/library/img.png" class="left">
-    <p>    Cras neque mauris, tempus tristique magna congue, luctus fermentum magna. Nulla a justo vel arcu facilisis elementum. Duis dui ex, dapibus ac hendrerit ut, tincidunt at ante. Vivamus euismod erat vel est volutpat consectetur. Fusce in egestas sem. In facilisis nunc nec eleifend ullamcorper. Etiam eget tellus fringilla, consequat sem nec, bibendum sem. In at purus non dui tincidunt malesuada a ac lorem. Donec sit amet porta est. Nunc massa nibh, suscipit sit amet pharetra eget, volutpat sit amet orci. Fusce ac aliquam nisi. Donec condimentum efficitur efficitur. Sed vitae ipsum imperdiet, pretium eros tristique, vestibulum lacus. Pellentesque imperdiet odio eget quam consectetur convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean venenatis in massa at mattis.
-    </p>
-    <p>    Morbi ultricies eros ante, at varius mauris commodo non. In id arcu blandit, egestas diam in, scelerisque ex. Nullam vitae volutpat felis. Etiam eget nunc at leo tempus dictum. Nullam vel ultricies justo. Nulla egestas non odio a sollicitudin. Suspendisse mattis, arcu in pharetra condimentum, ante nunc rutrum nunc, consequat ornare dolor tortor at lectus. Integer fringilla luctus eros, non cursus eros tristique sed. Nulla eget mattis leo.
-      </p>
+<?php get_header(); ?>
+
+<main role="article">
+
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+    <header>
+      <?php echo category_name(); ?>
+
+      <h1><?php the_title(); ?></h1>
+
+      <p class="meta"><?php echo get_the_author(); ?></p>
+    </header>
+    
+    <?php the_content(); ?>
 
     <div class="tags">
       <h4>Tags</h4>
@@ -24,8 +23,10 @@
       <h3>Comments</h3>
       <div style="height:20rem;background:silver"></div>
     </div>
-  </article>
-</main>
+      
+  </main>
 
-  <?php include 'footer.php'; ?>
+  <?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
 
