4af37028b9018580916eb0177be0ea3cc1fe00d6
K Belardinelli <k@kangabell.co>
2016-12-08 16:17:52 -0500

    header and main nav styling

--- partials/nav.php
+++ partials/nav.php
@@ -3,41 +3,40 @@
 </div>
 
 <header id="header">
-
-  <a href="index.php">
+  <div>
+    <a class="toggle-nav" href="javascript:void(0)">&#8801;</a> 
     <h1>
-      <a class="toggle-nav" href="javascript:void(0)" style="font-family: sans-serif;">&#8801;</a> 
-      <a href="<?php echo get_home_url(); ?>">Boston Hassle</a>
+      <a class="logo" href="<?php echo get_home_url(); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/library/hassle-logo.png"></a>
     </h1>
-  </a>
-
-  <nav>
-
-    <button class="toggle-nav">x Close</button>
-
-    <section>
-      <a href="">Calendar</a>
-    </section>
-
-    <section class="categories">
-      <a href="archive.php">New Music</a>
-      <a href="archive.php">Art</a>
-      <a href="archive.php">Film Flam</a>
-    </section>
-
-    <section class="pages">
-      <a href="">About</a>
-      <a href="">Boston Compass</a>
-      <a href="">Black Market</a>
-      <a href="">Web Market</a>
-      <a href="">Telethon</a>
-    </section>
-
-    <section class="buttons">
-      <button>Donate!</button><br />
-      <button>Volunteer!</button>
-    </section>
-
-  </nav>
+  </div>
 
 </header>
+
+<nav>
+
+  <button class="toggle-nav">x Close</button>
+
+  <section>
+    <a href="/events">Calendar</a>
+  </section>
+
+  <section class="categories">
+    <a href="archive.php">New Music</a>
+    <a href="archive.php">Art</a>
+    <a href="archive.php">Film Flam</a>
+  </section>
+
+  <section class="pages">
+    <a href="">About</a>
+    <a href="">Boston Compass</a>
+    <a href="">Black Market</a>
+    <a href="">Web Market</a>
+    <a href="">Telethon</a>
+  </section>
+
+  <section class="buttons">
+    <button>Donate!</button><br />
+    <button>Volunteer!</button>
+  </section>
+
+</nav>
