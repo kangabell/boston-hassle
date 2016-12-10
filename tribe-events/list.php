bdb902eb667b97a469df0afaa825bcaf14df9481
K Belardinelli <k@kangabell.co>
2016-12-10 15:05:07 -0500

    get filters functionality functioning

--- tribe-events/list.php
+++ tribe-events/list.php
@@ -10,23 +10,39 @@ if ( ! defined( 'ABSPATH' ) ) {
 do_action( 'tribe_events_before_template' );
 ?>
 
+  <!-- Tribe Bar -->
+<?php tribe_get_template_part( 'modules/bar' ); ?>
+
+
 <main>
   
   <header>
     <h1><?php echo tribe_get_events_title(); ?></h1>
   </header>
 
-  <div class="left">
+  <div id="tribe-events-content" class="left">
 
-  	<?php
-  	  if (have_posts()) : while (have_posts()) : the_post();
-  	    get_template_part('partials/event');
-  	  endwhile; endif;
-  	?>
+      <?php
+      /**
+       * Fires before any content is printed inside the list view.
+       */
+      do_action( 'tribe_events_list_before_the_content' );
+      ?>
+
+      <!-- Events Loop -->
+      <?php if ( have_posts() ) : ?>
+        <?php do_action( 'tribe_events_before_loop' ); ?>
+        <?php tribe_get_template_part( 'list/loop' ) ?>
+        <?php do_action( 'tribe_events_after_loop' ); ?>
+      <?php endif; ?>
+
+      </div>
+      <!-- #tribe-events-footer -->
+      <?php do_action( 'tribe_events_after_footer' ) ?>
 
   </div>
 
-  <?php get_sidebar('events'); ?>
+  <?php //get_sidebar('events'); ?>
 
 </main>
 <?php
