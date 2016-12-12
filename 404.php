07d74c9246ae528b8a89c706f9b4c540fe19f0f7
K Belardinelli <k@kangabell.co>
2016-12-12 14:58:43 -0500

    create 404 page

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<main>
+
+    <header>
+      <h1>404: Page Not Found</h1>
+    </header>
+
+    <article>
+    
+    	<p>Sorry, dude.</p>
+
+    </article>
+      
+  </main>
+
+<?php get_footer(); ?>
