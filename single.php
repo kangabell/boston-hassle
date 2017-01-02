243e7cb1282adbd2bcb5bf6bd3511ae5b29a873a
K Belardinelli <k@kangabell.co>
2017-01-02 18:03:35 -0500

    add subheading custom field to single post page

--- single.php
+++ single.php
@@ -13,6 +13,12 @@
 
       <h1><?php the_title(); ?></h1>
 
+      <?php if ( get_field('subtitle') ) : ?>
+        <p class="subheading">
+            <?php the_field('subtitle'); ?>
+        </p>
+      <?php endif; ?>
+
       <p class="meta">by <?php the_author_posts_link(); ?></p>
 
     </header>
