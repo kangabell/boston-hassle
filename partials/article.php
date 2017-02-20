9dbb8df1fc5af5ae0ffdc3f9e3b0bb69f44dae5c
K Belardinelli <k@kangabell.co>
2017-02-20 17:35:06 -0500

    related posts section on single post page

--- partials/article.php
+++ partials/article.php
@@ -1,14 +1,14 @@
 <!--
 MEDIUM-SIZED ARTICLE SNIPPET
-used on the homepage, archive pages, event archive pages, and search results pages
+used on the homepage, archive pages, event archive pages, search results pages, and related posts
 -->
 
-<article class="<?php echo $class; ?>"><a href="<?php the_permalink(); ?>">
+<article class="<?php if ( isset($class) ) { echo $class; } ?>"><a href="<?php the_permalink(); ?>">
 
-	<?php if ( $class != 'archive' ) { get_template_part('partials/date-category'); } ?>
+	<?php if ( (isset($class)) && ($class != 'archive') ) { get_template_part('partials/date-category'); } ?>
 	<div class="thumbnail" style="background-image: url('<?php the_post_thumbnail_url('grid-thumb'); ?>')"></div>
 	<div class="text">
-		<?php if ( $class == 'archive' ) { get_template_part('partials/date-category'); } ?>
+		<?php if ( (isset($class)) && ($class == 'archive') ) { get_template_part('partials/date-category'); } ?>
 		<h3><?php echo short_title(); ?></h3>
 		<?php the_excerpt(); ?>
 		<p class="meta"><?php echo get_the_author(); ?></p>
