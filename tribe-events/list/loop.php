3ed89f214ab7191e71e7d8a051db8e557b40cec5
kangabell <k@kangabell.co>
2019-01-14 13:33:23 -0500

    use our own single event partial

--- tribe-events/list/loop.php
+++ tribe-events/list/loop.php
@@ -30,25 +30,16 @@ $more = false;
 
 		<!-- Event  -->
 		<?php
-		$post_parent = '';
-		if ( $post->post_parent ) {
-			$post_parent = ' data-parent-post-id="' . absint( $post->post_parent ) . '"';
+
+		if ( tribe_event_in_category('hassle-shows') ) {
+			$hassle_show = true;
+		} else {
+			$hassle_show = false;
 		}
+
+		include(locate_template('partials/event.php'));
+
 		?>
-		<div id="post-<?php the_ID() ?>" class="<?php tribe_events_event_classes() ?>" <?php echo $post_parent; ?>>
-			<?php
-			$event_type = tribe( 'tec.featured_events' )->is_featured( $post->ID ) ? 'featured' : 'event';
-
-			/**
-			 * Filters the event type used when selecting a template to render
-			 *
-			 * @param $event_type
-			 */
-			$event_type = apply_filters( 'tribe_events_list_view_event_type', $event_type );
-
-			tribe_get_template_part( 'list/single', $event_type );
-			?>
-		</div>
 
 
 		<?php do_action( 'tribe_events_inside_after_loop' ); ?>
