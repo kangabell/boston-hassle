55c6139205bc511ccac7f20d393cd88d4cdb6b36
kangabell <k@kangabell.co>
2019-01-14 13:50:34 -0500

    update events pagination

--- partials/pagination.php
+++ partials/pagination.php
@@ -4,22 +4,10 @@ used on archive, events archive, and search-results pages
 -->
 
 <nav class="pagination">
-    <span class="prev-link">
-	<?php 
-		if ( tribe_is_event_query() ) { // if it's an event page
-			previous_posts_link(__('&#8592; Earlier', 'bhass'));
-		} else {
-    		next_posts_link(__('&#8592; Older', 'bhass'));
-		}
-	?>
-    </span>
-    <span class="next-link">
-		<?php 
-			if ( tribe_is_event_query() ) { // if it's an event page
-				next_posts_link(__('Later &#8594;', 'bhass'));
-			} else {
-	    		previous_posts_link(__('Newer &#8594;', 'bhass'));
-			}
-		?>
-    </span>
-</nav> 
+	<span class="prev-link">
+		<?php next_posts_link(__('&#8592; Older', 'bhass')); ?>
+	</span>
+	<span class="next-link">
+		<?php previous_posts_link(__('Newer &#8594;', 'bhass')); ?>
+	</span>
+</nav>
